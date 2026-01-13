64e3cb90033c8e680156c872b0c4e8d158998ca8
viralpraxis <iaroslav2k@gmail.com>
2026-01-13 22:02:12 +0400

    Fix RuboCop offenses

--- lib/activejob-unique.rb
+++ lib/activejob-unique.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-# rubocop:disable Naming/FileName
 # This file intentionally uses a dash to match the gem name 'activejob-unique'
 require 'active_job/uniqueness'
-# rubocop:enable Naming/FileName
